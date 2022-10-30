abe198c6eb996f3e7e9ee387a3da00e45a66f28d
louart13 <98977551+louart13@users.noreply.github.com>
2022-10-30 17:06:17 +0100

    Initial commit

---
+++ sddff.cpp
@@ -0,0 +1,15 @@
+//
+// Created by louar on 30/10/2022.
+//
+
+typedef struct{
+    int nbff;
+}verbsff;        // structure de données des formes fléchies des verbes;
+
+typedef struct{
+    int nbff;
+}adjectifff;     // structure de données des formes fléchies des adjectifs;
+
+typedef struct{
+    int nbff;
+}nomff;          // structure de données des formes fléchies des noms;
---
+++ sddff.h
@@ -0,0 +1,20 @@
+//
+// Created by louar on 30/10/2022.
+//
+
+#ifndef UNTITLED4_SDDFF_H
+#define UNTITLED4_SDDFF_H
+
+#endif //UNTITLED4_SDDFF_H
+
+typedef struct{
+    int nbff;
+}verbsff;        // structure de données des formes fléchies des verbes;
+
+typedef struct{
+    int nbff;
+}adjectifff;     // structure de données des formes fléchies des adjectifs;
+
+typedef struct{
+    int nbff;
+}nomff;          // structure de données des formes fléchies des noms;
